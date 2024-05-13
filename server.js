db7023893b4dc6a5c64f66117f81e61018deafd4
Desmond Obinna <talk2desmond91@gmail.com>
2024-05-14 00:02:34 +0100

    added a post request

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const http = require('http');
-const fs = require('fs')
 
 const PORT = 8000;
+let db = [];
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/plain'})
-    res.end('this purpose server')
-    
+    if(req.url === "/" && req.method === "GET"){
+        res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify(db));
+    }
 
-})
+    if(req.url === '/' && req.method === 'POST'){
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end',()=>{
+            const {title, comedian,year} = JSON.parse(body)
+            const newjoke = {
+                title,
+                comedian,
+                year
+            }
+
+            db.push(newjoke);
+            res.writeHead(201, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify(db));
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found')
+    }
+
+});
 
 server.listen(PORT,'localhost', ()=>{
     console.log("Server listeniing on port 8000")
